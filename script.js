4543aa3bb40539cacaf47168690d18d29671fe22
ramu siripalli <ramsiripalli2772@gmail.com>
2026-01-06 11:12:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+let firstName = document.querySelector("#fname").value;
+let lastName = document.querySelector("#lname").value;
+	alert(`${firstName} ${lastName}`);
 }
