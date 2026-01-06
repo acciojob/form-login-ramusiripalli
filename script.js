7741ecdbb2a072a42f0a51c4b15012045f96ffb1
ramu siripalli <ramsiripalli2772@gmail.com>
2026-01-06 11:22:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
-let firstName = document.querySelector("#fname").value;
-let lastName = document.querySelector("#lname").value;
+	event.preventDefault();
+let firstName = document.querySelector("#fname").value.trim();
+let lastName = document.querySelector("#lname").value.trim();
+	if(firstName === "" || lastName === ""){
+		alert("please enter both first and last name");
+		return;
+	}
+	let namePattern = /^[A-Za-z\s'-]+$/;
+	if(!namePattern.test(firstName) || !namePattern.test(lastName)){
+		 alert("Names can contain only letters, spaces, hyphens, and apostrophes");
+        return;
+	}
 	alert(`${firstName} ${lastName}`);
 }
